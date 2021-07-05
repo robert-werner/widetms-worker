1bf712f21a78845340f8ba4ebcf5d51c1db8ca00
Leonid Kolesnichenko <xperience439@gmail.com>
2021-07-05 11:38:44 +0300

    -- Add exceptions

--- widetms/worker.py
+++ widetms/worker.py
@@ -10,20 +10,23 @@ from widetms.tile_scalers.scaler import scale_tile
 
 @app.task()
 def tile(task):
-    filepaths = [os.path.join(SOURCE_STORAGE, filepath) for filepath in task['filepaths']]
-    print(filepaths)
-    band_package = list(zip(filepaths, task['bands']))
-    with BandPackageReader(band_package, reader_options={'resampling_method': task['resampling']}) as georaster:
-        _tile = georaster.tile(task['x'],
-                               task['y'],
-                               task['z'],
-                               tilesize=task['resolution'],
-                               bands=georaster.bands,
-                               expression=task['expressions'])
-        if task['expressions'] is None:
-            if numpy.dtype(task['dtype']) == _tile.data[0].dtype:
-                return _tile.data[0]
+    try:
+        filepaths = [os.path.join(SOURCE_STORAGE, filepath) for filepath in task['filepaths']]
+        band_package = list(zip(filepaths, task['bands']))
+        with BandPackageReader(band_package, reader_options={'resampling_method': task['resampling']}) as georaster:
+            _tile = georaster.tile(task['x'],
+                                   task['y'],
+                                   task['z'],
+                                   tilesize=task['resolution'],
+                                   bands=georaster.bands,
+                                   expression=task['expressions'])
+            if task['expressions'] is None:
+                if numpy.dtype(task['dtype']) == _tile.data[0].dtype:
+                    return _tile.data[0]
+                else:
+                    return scale_tile(task['rs_device'], _tile.data[0], task['dtype'])
             else:
-                return scale_tile(task['rs_device'], _tile.data[0], task['dtype'])
-        else:
-            return scale_tile(task['rs_device'], _tile.data[0])
+                return scale_tile(task['rs_device'], _tile.data[0])
+    except Exception as e:
+        raise Exception(str(e))
+
